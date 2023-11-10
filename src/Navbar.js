85f15b66ee0915fa3b9468bee5f1d2d9a5f51f47
Seb Bains <sebastianbains@gmail.com>
2023-11-10 16:04:35 +0000

    added material ui & select format

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
 class Navbar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {format: "hex"}
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e){
+    this.setState({format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const {level, changeLevel} = this.props;
+    const {format} = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -16,6 +28,13 @@ class Navbar extends Component {
             <div className="slider">
             <Slider defaultValue={level} min={100} max={900} step={100} onChange={changeLevel}/>
             </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #fff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 1)</MenuItem>
+          </Select>
         </div>
       </header>
     )
